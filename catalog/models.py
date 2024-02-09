781d222f927e5b56464dd63ee9dae2faa3e263c8
regisatl <regisattolou21@outlook.fr>
2024-02-09 10:31:25 +0100

    implementation du models catalog

--- catalog/models.py
+++ catalog/models.py
@@ -3,5 +3,12 @@ from django.db import models
 # Create your models here.
 
 class Book(models.Model):
-      title = models.CharField(max_length=255, help_text='Enter the title')
-      
+    title = models.CharField(max_length=255, help_text='Enter the title')
+    author = models.CharField(max_length=255, help_text='Enter the author name')
+    summary = models.TextField(help_text='Enter a brief description of the book')
+    cover = models.ImageField(upload_to='covers/', blank=True)
+    price = models.DecimalField(max_digits=5, decimal_places=2, help_text='Enter the price of the book')
+    publish_date = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        return self.title
--- library/settings.py
+++ library/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'catalog.apps.CatalogConfig',
 ]
 
 MIDDLEWARE = [
@@ -75,10 +76,15 @@ WSGI_APPLICATION = 'library.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'library',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',   # Ou l'adresse IP de votre serveur de base de données
+        'PORT': '3306',        # Le port par défaut de MySQL est 3306
     }
 }
+
 
 
 # Password validation
