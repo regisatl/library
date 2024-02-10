9097593d0fbe68572604c42af0afe859b3cd096e
regisatl <regisattolou21@outlook.fr>
2024-02-10 14:35:51 +0100

    fin du commit pour django

--- library/settings.py
+++ library/settings.py
@@ -127,8 +127,8 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media/'
 
 
 # Default primary key field type
